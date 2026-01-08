109c63e0b157b11ba51545559d436e2ccfa45c53
Krishna Mahajan <krishmahajanlalit@gmail.com>
2026-01-08 23:49:47 +0530

    classes

---
+++ learnjava/src/packconstructor/InnerDemo.java
@@ -0,0 +1,19 @@
+
+package packconstructor;
+
+class Outer {
+    int a = 100;
+
+    class Inner {
+        void toprint() {
+            System.out.println("a = " + a);
+        }
+    }
+}
+
+public class InnerDemo {
+    public static void main(String[] args) {
+        Outer.Inner innerobj = new Outer().new Inner();
+        innerobj.toprint();
+    }
+}
