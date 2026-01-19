ace393f0a77d6bf8cb3cb5657292b0b1ab4e975f
Krishna Mahajan <krishmahajanlalit@gmail.com>
2026-01-19 23:00:39 +0530

    random math lang pack used for otps

---
+++ learnjava/src/randommath.java
@@ -0,0 +1,9 @@
+
+public class randommath {
+
+	public static void main(String[] args) {
+		System.out.println(Math.random());
+		System.out.println((int)(Math.random() * 10000));
+		System.out.println((int)(Math.random() * 1000000));
+} }
+
