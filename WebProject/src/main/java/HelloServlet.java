14f45e927463a4b9ece9cc28b8c335922db3f2aa
Krishna Mahajan <krishmahajanlalit@gmail.com>
2026-03-07 16:10:27 +0530

    Configured Tomcat server and implemented HelloServlet servlet example

---
+++ WebProject/src/main/java/HelloServlet.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+
+        out.println("Hello World");
+        out.println("<font face='arial' size='5' color='red'>Good Morning</font>");
+    }
+}
